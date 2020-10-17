841a6190ee93d6dadedd5a0b597027a1bed3f530
sakilk130 <sakilk130@gmail.com>
2020-10-17 22:36:31 +0600

    add react-typed & display it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Typography, Avatar, Grid, Box } from '@material-ui/core';
 import avatar from '../image/myImage.jpg';
+import Typed from 'react-typed';
 
 const Header = () => {
   return (
     <Box>
       <Avatar src={avatar} alt="Sakil Khan" />
+      <Typography variant="h4">
+        <Typed strings={['Sakil Khan']} typeSpeed={100} />
+      </Typography>
+      <br />
+      <Typography variant="h5">
+        <Typed
+          strings={[
+            'JavaScript Developer',
+            'MERN Stack Developer',
+            'Web Developer',
+          ]}
+          typeSpeed={70}
+          backSpeed={60}
+          loop
+        />
+      </Typography>
     </Box>
   );
 };
