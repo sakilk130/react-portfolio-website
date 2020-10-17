b5b56d99e5a652bb261f392ad1cb8a007c16fa0e
sakilk130 <sakilk130@gmail.com>
2020-10-17 22:57:42 +0600

    add style on Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,46 @@ import React from 'react';
 import { Typography, Avatar, Grid, Box } from '@material-ui/core';
 import avatar from '../image/myImage.jpg';
 import Typed from 'react-typed';
+import { makeStyles } from '@material-ui/core/styles';
+
+//css style
+const useStyles = makeStyles((theme) => ({
+  avater: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    margin: theme.spacing(1),
+  },
+  title: {
+    color: 'tomato',
+  },
+  subtitle: {
+    color: 'tan',
+    marginBottom: '3rem',
+  },
+  typedContainer: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '100vw',
+    textAlign: 'center',
+    zIndex: 1,
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+
   return (
-    <Box>
-      <Avatar src={avatar} alt="Sakil Khan" />
-      <Typography variant="h4">
+    <Box className={classes.typedContainer}>
+      <Grid container justify="center">
+        <Avatar className={classes.avater} src={avatar} alt="Sakil Khan" />
+      </Grid>
+      <Typography className={classes.title} variant="h4">
         <Typed strings={['Sakil Khan']} typeSpeed={100} />
       </Typography>
       <br />
-      <Typography variant="h5">
+      <Typography className={classes.subtitle} variant="h5">
         <Typed
           strings={[
             'JavaScript Developer',
