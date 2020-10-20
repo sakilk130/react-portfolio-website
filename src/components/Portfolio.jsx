e3d78a3097ccf2f983d51563d6f38e049791ada7
sakilk130 <sakilk130@gmail.com>
2020-10-21 01:06:13 +0600

    update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,7 +11,8 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-import project1 from '../image/reactWithFirebase.png';
+import project1 from '../image/todoAppReactWithFirebase.png';
+import project2 from '../image/reactWeatherApp.png';
 
 const useStyles = makeStyles((theme) => ({
   mainContainer: {
@@ -21,56 +22,75 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: 345,
     margin: '3rem auto',
   },
+  projectName: {
+    color: 'tomato',
+  },
+  projectDescription: {
+    color: 'rgb(132 29 2 / 1)',
+  },
+  button: {
+    backgroundColor: 'tan',
+  },
+  liveButton: {
+    backgroundColor: '#44c767',
+    borderRadius: '28px',
+    border: '1px solid #18ab29',
+    display: 'inline-block',
+    cursor: 'pointer',
+    color: '#ffffff',
+    fontSize: '13px',
+    padding: '3px 8px',
+    textDecoration: 'none',
+    textShadow: '0px 1px 0px #2f6627',
+    '&:hover': {
+      backgroundColor: '#5cbf2a',
+    },
+    '&:active': {
+      position: 'relative',
+      top: '1px',
+    },
+  },
+  sourceButton: {
+    backgroundColor: '#7892c2',
+    borderRadius: '28px',
+    border: '1px solid #4e6096',
+    display: 'inline-block',
+    cursor: 'pointer',
+    color: '#ffffff',
+    fontSize: '13px',
+    padding: '3px 8px',
+    textDecoration: 'none',
+    textShadow: '0px 1px 0px #283966',
+    '&:hover': {
+      backgroundColor: '#476e9e',
+    },
+    '&:active': {
+      position: 'relative',
+      top: '1px',
+    },
+  },
 }));
 
 const projects = [
   {
-    name: 'Todo App Using React With Firebase',
+    name: 'Todo App',
     description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
     consequatur magni quod nesciunt necessitatibus molestiae non
     eligendi, magnam est aliquam recusandae? Magnam soluta minus
     iste alias sunt veritatis nisi dolores!`,
     image: project1,
+    live: 'https://react-firebase-todo-app-d37d1.web.app/',
+    source: 'https://github.com/sakilk130/react-firebase-todo-app',
   },
   {
-    name: 'Project 2',
+    name: 'Weather App',
     description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
     consequatur magni quod nesciunt necessitatibus molestiae non\
     eligendi, magnam est aliquam recusandae? Magnam soluta minus\
     iste alias sunt veritatis nisi dolores!`,
-    image: 'project2',
-  },
-  {
-    name: 'Project 3',
-    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
-    consequatur magni quod nesciunt necessitatibus molestiae non\
-    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
-    iste alias sunt veritatis nisi dolores!`,
-    image: 'project3',
-  },
-  {
-    name: 'Project 4',
-    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
-    consequatur magni quod nesciunt necessitatibus molestiae non\
-    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
-    iste alias sunt veritatis nisi dolores!`,
-    image: 'project4',
-  },
-  {
-    name: 'Project 5',
-    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
-    consequatur magni quod nesciunt necessitatibus molestiae non\
-    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
-    iste alias sunt veritatis nisi dolores!`,
-    image: 'project5',
-  },
-  {
-    name: 'Project 6',
-    description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
-    consequatur magni quod nesciunt necessitatibus molestiae non\
-    eligendi, magnam est aliquam recusandae? Magnam soluta minus\
-    iste alias sunt veritatis nisi dolores!`,
-    image: 'project6',
+    image: project2,
+    live: 'https://react-weather-app-v1.netlify.app/',
+    source: 'https://github.com/sakilk130/react-weather-app',
   },
 ];
 
@@ -93,20 +113,40 @@ const Portfolio = () => {
                     image={project.image}
                   />
                   <CardContent>
-                    <Typography variant="h5" gutterBottom>
+                    <Typography
+                      variant="h5"
+                      gutterBottom
+                      className={classes.projectName}
+                    >
                       {project.name}
                     </Typography>
-                    <Typography variant="body2" color="textSecondary">
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      className={classes.projectDescription}
+                    >
                       {project.description}
                     </Typography>
                   </CardContent>
                 </CardActionArea>
-                <CardActions>
-                  <Button size="small" color="primary">
-                    Share
-                  </Button>
-                  <Button size="small" color="primary">
+                <CardActions className={classes.button}>
+                  <Button
+                    size="small"
+                    color="primary"
+                    className={classes.liveButton}
+                    target="blank"
+                    href={project.live}
+                  >
                     Live Demo
+                  </Button>
+                  <Button
+                    size="small"
+                    color="primary"
+                    className={classes.sourceButton}
+                    target="blank"
+                    href={project.source}
+                  >
+                    Source Code
                   </Button>
                 </CardActions>
               </Card>
