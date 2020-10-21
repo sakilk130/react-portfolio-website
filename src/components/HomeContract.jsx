b9d96c8e6a522b275c7879773ea3250db94ff810
sakilk130 <sakilk130@gmail.com>
2020-10-21 22:46:47 +0600

    social media linkup to HomeContract

--- src/components/HomeContract.jsx
+++ src/components/HomeContract.jsx
@@ -19,14 +19,37 @@ const useStyles = makeStyles({
     },
   },
 });
-
+// data
+const contracts = [
+  {
+    id: 1,
+    name: 'GitHub',
+    icon: GitHubIcon,
+    username: 'sakilk130',
+    link: 'https://github.com/sakilk130',
+  },
+  {
+    id: 2,
+    name: 'LinkedIn',
+    icon: LinkedInIcon,
+    username: 'sakil-khan-076926145',
+    link: 'https://www.linkedin.com/in/sakil-khan-076926145/',
+  },
+];
 function HomeContract() {
   const classes = useStyles();
   return (
     <>
       <BottomNavigation className={classes.root} style={{ background: 'none' }}>
-        <BottomNavigationAction icon={<GitHubIcon />} />
-        <BottomNavigationAction icon={<LinkedInIcon />} />
+        {contracts.map((contract) => (
+          <BottomNavigationAction
+            key={contract.id}
+            icon={<contract.icon />}
+            target="blank"
+            href={contract.link}
+            title={contract.username}
+          />
+        ))}
       </BottomNavigation>
     </>
   );
