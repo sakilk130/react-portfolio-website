ae39a19675ac73acdd105151cddc613e7d246f44
sakilk130 <sakilk130@gmail.com>
2020-10-23 20:28:13 +0600

    add more skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,7 +10,6 @@ import {
   CardContent,
   Grid,
 } from '@material-ui/core';
-import StarIcon from '@material-ui/icons/Star';
 
 const useStyles = makeStyles({
   heading: {
@@ -35,8 +34,9 @@ const useStyles = makeStyles({
   },
   language: {
     background: 'white',
-    margin: '0.2rem auto',
+    margin: '1.5rem auto',
     borderRadius: '0.5rem ',
+    padding: '0.5rem',
   },
   languageView: {
     display: 'flex',
@@ -45,29 +45,84 @@ const useStyles = makeStyles({
 
 const skills = [
   {
-    title: 'C++',
-    logo: 'https://seeklogo.com/images/C/c-logo-1B1817C041-seeklogo.com.png',
-  },
-  {
     title: 'HTML',
     logo: 'https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg',
-  },
-  {
-    title: 'CSS',
-    logo: 'https://seeklogo.com/images/C/css3-logo-F1923C8D0E-seeklogo.com.png',
   },
   {
     title: 'JavaScript',
     logo: 'https://www.vectorlogo.zone/logos/javascript/javascript-icon.svg',
   },
   {
+    title: 'MongoDB',
+    logo: 'https://www.vectorlogo.zone/logos/mongodb/mongodb-ar21.svg',
+  },
+
+  {
+    title: 'CSS',
+    logo: 'https://seeklogo.com/images/C/css3-logo-F1923C8D0E-seeklogo.com.png',
+  },
+  {
+    title: 'Firebase',
+    logo: 'https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg',
+  },
+  {
     title: 'TypeScript',
     logo:
       'https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg',
   },
+
   {
     title: 'PHP',
     logo: 'https://www.vectorlogo.zone/logos/php/php-icon.svg',
+  },
+
+  {
+    title: 'Bootstrap',
+    logo:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png',
+  },
+
+  {
+    title: 'ReactJS',
+    logo: 'https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg',
+  },
+
+  {
+    title: 'Material-UI',
+    logo:
+      'https://camo.githubusercontent.com/cf05625198fe7b6ad8a302d1ce16bc99b93ec2ac/68747470733a2f2f6d6174657269616c2d75692e636f6d2f7374617469632f6c6f676f2e737667',
+  },
+
+  {
+    title: 'NodeJS',
+    logo: 'https://www.vectorlogo.zone/logos/nodejs/nodejs-icon.svg',
+  },
+
+  {
+    title: 'Git',
+    logo: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg',
+  },
+
+  {
+    title: 'GitHub',
+    logo: 'https://www.vectorlogo.zone/logos/github/github-icon.svg',
+  },
+  {
+    title: 'ExpressJS',
+    logo: 'https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg',
+  },
+  {
+    title: 'VS Code',
+    logo: 'https://cdn.worldvectorlogo.com/logos/visual-studio-code.svg',
+  },
+  {
+    title: 'npm',
+    logo: 'https://www.vectorlogo.zone/logos/npmjs/npmjs-icon.svg',
+  },
+  {
+    title: 'Linux',
+    logo:
+      'https://www.freepnglogos.com/uploads/linux-png/file-icons-flat-linux-svg-wikimedia-commons-6.png',
   },
 ];
 function Skills() {
@@ -80,10 +135,6 @@ function Skills() {
           MY SKILLS
         </Typography>
         <div className={classes.language}>
-          <Typography variant="p" className={classes.projectName}>
-            <StarIcon style={{ color: 'black' }} />
-            Programming/Markup Languages
-          </Typography>
           <Grid container justify="center">
             {skills.map((skill, i) => (
               <Grid item xs={12} sm={8} md={4} key={i}>
